a437be7cf9135bba226b5c437dec87da6b2056b6
Jan Tabara <tabara.jan@seznam.cz>
2018-09-10 13:40:36 +0200

    basic delegate

--- Events/Program.cs
+++ Events/Program.cs
@@ -4,9 +4,36 @@ namespace Events
 {
     class Program
     {
+        // declaration of a delegate
+        // Handler suffix is a good practice
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // instantiate the delegate
+            // tell it what method should it call
+            var del = new WorkPerformedHandler(WorkPerformed);
+            var del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            // execute the delegate
+            del(2, WorkType.MakePizza);
         }
+
+        static void WorkPerformed(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Working on: {workType.ToString()} for {hours} hours");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Working on: {workType.ToString()} for {hours} hours | work2");
+        }
+    }
+
+    public enum WorkType
+    {
+        MakePizza,
+        DoNothing,
+        GenerateReports
     }
 }
