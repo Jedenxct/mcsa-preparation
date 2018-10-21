83c13a550040691947a6ea7b6833f5b04541d44e
Elevxnth <tabara.jan@seznam.cz>
2018-10-21 13:59:43 +0200

    threading start boy

---
+++ Threading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // thread takes a delegate of an entry point method
+            var thread = new Thread(NewMain);
+            thread.Start();
+
+            // parametrized
+            var parametrizedThread = new Thread(ParametrizedMethod);
+            var processorCount = Environment.ProcessorCount;
+            parametrizedThread.Start(processorCount);
+        }
+
+        static void NewMain()
+        {
+            Console.WriteLine("Main has moved here huh.");
+        }
+
+        static void ParametrizedMethod(object parameter)
+        {
+            Console.WriteLine("Processor count: " + parameter);
+        }
+    }
+}
