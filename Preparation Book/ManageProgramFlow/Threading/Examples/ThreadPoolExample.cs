cc44ba599604afcba3cecaf869e6ab12a3af802a
Jan Tabara <tabara.jan@seznam.cz>
2018-12-14 13:51:12 +0100

    thread pool

---
+++ Preparation Book/ManageProgramFlow/Threading/Examples/ThreadPoolExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Threading.Interfaces;
+
+namespace Threading.Examples
+{
+    public class ThreadPoolExample : IExample
+    {
+        // instead of letting the threads die, it is a good idea to keep them in the pool
+        // every time new task is scheduled, the pool dedicates/reuses existing thread that was inactive
+
+        public void Run()
+        {
+            // what is a?
+            ThreadPool.QueueUserWorkItem((a) => Console.WriteLine("Previously queued, now running."));
+        }
+  
+    }
+}
