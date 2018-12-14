c7959a046bba92df4ef4b47ce9363b68dc1dd1b7
Jan Tabara <tabara.jan@seznam.cz>
2018-12-14 11:33:30 +0100

    few things on tuples

---
+++ Tuples/Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unnamed tuple
+            var unnamed = (2, "Franto", 3);
+
+            Console.WriteLine(unnamed.Item1);
+            Console.WriteLine(unnamed);
+
+            // named tuple
+            var named = (age: 23, name: "Franto");
+
+            // named tuples have aliases for properties
+            Console.WriteLine(named.Item1);
+            Console.WriteLine(named.age);
+            Console.WriteLine(named);
+        }
+    }
+}
